46a82b523a90e496b1c84856821c927afa8a9a84
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-05-07 15:52:18 +0800

    Add Loader test

---
+++ src/components/LoadingIndicator/tests/index.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+
+import 'jest-styled-components'
+
+import LoadingIndicator from '../index'
+
+describe('<LoadingIndicator />', () => {
+  it('should match the snapshot', () => {
+    const { container } = render(<LoadingIndicator />)
+    expect(container).toMatchSnapshot()
+  })
+})
