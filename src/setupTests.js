4c615c4f66e102fba5a1a054ac9e9ae23b8aaf8c
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-05-11 23:21:58 +0800

    Add hack rtl#281 remove warning till 16.8.9 is stable

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,26 @@
 
 import 'react-testing-library/cleanup-after-each'
 import 'jest-dom/extend-expect'
+
+/**
+ * Issue about the warning and with our test `containers/SkillsPage/tests/index.test.js
+ * https://github.com/testing-library/react-testing-library/issues/281#issuecomment-480349256
+ * 
+ * TLDR:
+ *  issue about warning of `act`
+ */
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+})
+
+// -- end of snippet
