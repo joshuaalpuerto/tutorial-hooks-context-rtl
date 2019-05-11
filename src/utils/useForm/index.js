a0f3f5f71c791fe43cf92ae3d95c7230a8661dc3
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-05-11 11:11:09 +0800

    Add hooks to manage form

---
+++ src/utils/useForm/index.js
@@ -0,0 +1,39 @@
+import  { useReducer, useCallback } from 'react'
+
+const INITIAL_STATE = {}
+function formReducer(state, action) {
+  switch (action.type) {
+    case '__reset__':
+      return INITIAL_STATE
+    case action.type:
+      return {
+        ...state,
+        [action.type]: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+/* eslint-disable react/prefer-stateless-function */
+const useForm = () => {
+  const [ values, dispatch ] = useReducer(formReducer, INITIAL_STATE)
+
+  const onChange = useCallback((evt) => {
+    const { name: type, value: payload } = evt.target
+
+    dispatch({ type, payload })
+  },[])
+
+  const resetForm = useCallback(() => {
+    dispatch({ type: '__reset__' })
+  },[])
+
+  return {
+    values,
+    onChange,
+    resetForm
+  }
+}
+
+export default useForm
